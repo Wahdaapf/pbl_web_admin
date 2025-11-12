d85c3555830f55341aa391c1e91fb4d8f94e9aaf
Lindaoctaviani7 <lindaoctaviani29@gmail.com>
2025-11-12 08:09:18 +0700

    fix bug

--- app/core/config.php
+++ app/core/config.php
@@ -1,32 +1,40 @@
 <?php
 
-if($_SERVER['HTTP_HOST'] == 'localhost') {
-  /* Local Database Connection */
-  define('DBHOST', 'localhost');
-  define('DBNAME', 'php_mvc');
-  define('DBUSER', 'root');
-  define('DBPASS', '');
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    /* Local Database Connection */
+    define('DBHOST', 'localhost');
+    define('DBNAME', 'php_mvc');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
 
-  /* Specify the default Url */
-  define('ROOT','http://localhost/fallax_projects/php_mvc_htmx/public');
+    /* Specify the default URL (auto detect folder) */
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443)
+        ? "https://" : "http://";
 
-  // true mean show errors
-  define('DEBUG', true);
-  
+    // Ambil path folder project otomatis
+    $rootPath = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+
+    define('ROOT', $protocol . $_SERVER['HTTP_HOST'] . $rootPath);
+
+    // true means show errors
+    define('DEBUG', true);
+
 } else {
-  /* Local Database Connection */
-  define('DBHOST', '');
-  define('DBNAME', '');
-  define('DBUSER', '');
-  define('DBPASS', '');
+    /* Production Database Connection */
+    define('DBHOST', '');
+    define('DBNAME', '');
+    define('DBUSER', '');
+    define('DBPASS', '');
 
-  /* Specify the default Url */
-  define('ROOT','http://localhost:8000');
+    /* Specify the default URL */
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443)
+        ? "https://" : "http://";
 
-  // false means don't show errors 
-  define('DEBUG', false);
+    define('ROOT', $protocol . $_SERVER['HTTP_HOST'] . '/');
+
+    // false means don't show errors 
+    define('DEBUG', false);
 }
-
 
 define('APP_NAME', 'My Website');
 define('APP_DESC', 'My Website Description');
